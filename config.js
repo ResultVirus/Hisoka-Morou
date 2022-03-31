92fc5ef424b489cbd78b234353954ac491fa90a0
ResultVirus <102714450+ResultVirus@users.noreply.github.com>
2022-03-31 16:42:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['62895397108647','6281252009563']
+global.premium = []
+global.packname = '中野樹 BOT'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
